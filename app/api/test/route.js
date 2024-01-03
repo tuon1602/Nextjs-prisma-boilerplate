e0f9172b036d26f095e785aac6f20e4384e3416c
nguyen tuan <tuan69878@gmail.com>
2024-01-03 21:02:56 +0700

    almost done

---
+++ app/api/test/route.js
@@ -0,0 +1,9 @@
+import { NextResponse } from "next/server";
+//import prisma client here
+export async function GET(){
+    //logic here
+    return NextResponse.json({message: "Hello World!"})
+}
+export async function POST(){
+
+}
